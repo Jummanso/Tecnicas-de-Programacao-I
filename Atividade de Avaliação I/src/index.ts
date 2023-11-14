7043b5e7cbe8521f9c0be24a20d843b8c19b4985
Juliana <julianamaciel97@gmail.com>
2023-11-14 19:39:52 -0300

    Atividade I

---
+++ Atividade de Avaliação I/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import route from './routes';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(route)
+
+app.use("*", (req, res) => { res.status(404).json({ message: "Requisição desconhecida" }) })
+
+app.listen(PORT, () => { console.log(`Server running at PORT:${PORT}`) })
